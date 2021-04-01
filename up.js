e8d95d75a0f5c9991bb92e0e37f5c0f500fb0b75
Julia <chehovskayulia@gmail.com>
2021-04-01 18:42:49 +0300

    first commit

---
+++ up.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+let toTopBtn = document.querySelector('.to-up');
+    window.onscroll = function () {
+        if (window.pageYOffset > 580) {
+            toTopBtn.style.display = 'block'
+        } else {
+            toTopBtn.style.display = 'none'
+        }
+    }
+    toTopBtn.addEventListener('click', function () {
+        window.scrollBy({
+            top: -document.documentElement.scrollHeight,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
